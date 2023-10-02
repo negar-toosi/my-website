ee125a4dc24d8c0156bcc1166754d7090a0647bf
negar-toosi <144216039+negar-toosi@users.noreply.github.com>
2023-10-02 13:23:41 +0330

    add record and attribute
    - create content and post table
    - add record to post table with shell and admin page

--- blog/models.py
+++ blog/models.py
@@ -2,5 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
+    #author
     title = models.CharField(max_length=255)
     content = models.TextField()
+    views = models.IntegerField(default=0)
+    #image
+    #category
+    status = models.BooleanField(default=False)
+    #tag
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField(null=True)
