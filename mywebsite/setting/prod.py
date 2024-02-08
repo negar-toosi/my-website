f3f2f01069df4ce9e7274338199cc86477115738
negar-toosi <144216039+negar-toosi@users.noreply.github.com>
2024-02-09 00:45:32 +0330

    create prod and dev files

---
+++ mywebsite/setting/prod.py
@@ -0,0 +1,33 @@
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-(lwzlbe2*7_^hiama%4i*(df+n(4t#6=#nujxe38w*@92ii$+2'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['negartoosi.ir','www.negartoosi.ir']
+# INSTALLED_APPS = []
+
+#sites framework
+SITE_ID = 2 
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+STATIC_ROOT = BASE_DIR / 'static'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+STATICFILES_DIRS = [BASE_DIR / "statics",]
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -16,28 +16,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(lwzlbe2*7_^hiama%4i*(df+n(4t#6=#nujxe38w*@92ii$+2'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
-# import os
-# if DEBUG:
-#     EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-#     EMAIL_HOST = 'smtp.gmail.com'
-#     EMAIL_POST = 587
-#     EMAIL_USE_TLS = True
-#     EMAIL_HOST_USER = os.environ.get('traces_email')
-#     EMAIL_HOST_PASSWORD = os.environ.get('traces_email_password') 
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -63,7 +41,7 @@ INSTALLED_APPS = [
     'password_reset',
     'crispy_forms',
 ]
-SITE_ID = 2 
+
 #captcha admin settings
 multi_captcha_admin = {
     'engine': 'simple-captcha',
@@ -108,14 +86,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mywebsite.wsgi.application'
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
@@ -157,14 +127,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
-STATIC_ROOT = BASE_DIR / 'static'
-MEDIA_ROOT = BASE_DIR / 'media'
 
-STATICFILES_DIRS = [BASE_DIR / "statics",]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-X_FRAME_OPTIONS = 'SAMEORIGIN'
+
 
 INTERNAL_IPS = [
 
